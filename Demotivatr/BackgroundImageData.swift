d7f0816e0ee78b44ad146093c78b4aec63e13806
Hurlarious <hurlarious@gmail.com>
2016-11-29 20:03:58 -0800

    podfile

---
+++ Demotivatr/BackgroundImageData.swift
@@ -0,0 +1,22 @@
+//
+//  BackgroundImageData.swift
+//  Demotivatr
+//
+//  Created by Dave Hurley on 2016-11-29.
+//  Copyright © 2016 Dave Hurley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class BackgroundImageData {
+    
+    var backgroundImage: UIImage!
+    
+    func getBackgoundImageData() {
+        
+    }
+    
+}
